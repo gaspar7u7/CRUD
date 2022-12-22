ee081e2e5a61373f81dac67f728e4045af63cc8d
gaspar <gaspar7u7@gmail.com>
2022-12-22 02:37:36 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,207 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Users from "./components/Users";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+import "./style.css";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const { register, handleSubmit, reset } = useForm();
+  const [isVisible, setIsVisible] = useState(false);
+
+  const clear = () => {
+    reset({
+      first_name: "",
+      last_name: "",
+      email: "",
+      birthday: "",
+      password: "",
+    });
+  };
+
+  useEffect(() => {
+    apiUsers("https://users-crud1.herokuapp.com/users/");
+  }, []);
+
+  const apiUsers = (url, method = "get", data = null) => {
+    switch (method) {
+      case "get": {
+        axios.get(url).then((res) => {
+          setUsers(res.data);
+        });
+        break;
+      }
+      case "post": {
+        axios.post(url, data).then(() => {
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "You added a new user",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          apiUsers("https://users-crud1.herokuapp.com/users/");
+        });
+        break;
+      }
+      case "put": {
+        axios
+          .put(url, data)
+          .then(() => {
+            Swal.fire({
+              position: "center",
+              icon: "success",
+              title: "You modified a user",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+            apiUsers("https://users-crud1.herokuapp.com/users/");
+          })
+          .catch((error) => console.log(error.response));
+        break;
+      }
+      case "delete": {
+        const swalWithBootstrapButtons = Swal.mixin({
+          customClass: {
+            confirmButton: "btn btn-success",
+            cancelButton: "btn btn-danger",
+          },
+          buttonsStyling: false,
+        });
+
+        swalWithBootstrapButtons
+          .fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Yes, delete it!",
+            cancelButtonText: "No, cancel!",
+            reverseButtons: true,
+          })
+          .then((result) => {
+            if (result.isConfirmed) {
+              axios
+                .delete(url)
+                .then(() => {
+                  apiUsers("https://users-crud1.herokuapp.com/users/");
+                })
+                .then(() => {
+                  swalWithBootstrapButtons.fire(
+                    "Deleted!",
+                    "The user has been deleted.",
+                    "success"
+                  );
+                });
+            } else if (result.dismiss === Swal.DismissReason.cancel) {
+              clear();
+
+              apiUsers("https://users-crud1.herokuapp.com/users/");
+              swalWithBootstrapButtons.fire(
+                "Cancelled",
+                "Your imaginary user is safe :)",
+                "error"
+              );
+            }
+          });
+
+        break;
+      }
+    }
+  };
+
+  const submit = (data) => {
+    apiUsers("https://users-crud1.herokuapp.com/users/", "post", data);
+    clear();
+    setIsVisible(!isVisible);
+  };
+
+  return (
+    <>
+      {isVisible ? (
+        <div className="background-create-user">
+          <div className="card-form">
+            <form
+              className="card-form-create-user"
+              onSubmit={handleSubmit(submit)}
+            >
+              <h1>Create Users</h1>
+
+              <label htmlFor="first_name">Fist name:</label>
+              <input
+                className="input input-text"
+                required
+                type="text"
+                id="first_name"
+                {...register("first_name")}
+              />
+
+              <label htmlFor="last_name">Last name:</label>
+
+              <input
+                required
+                type="text"
+                id="last_name"
+                {...register("last_name")}
+              />
+
+              <label htmlFor="birthday">
+                <i className="fa-solid fa-cake-candles"></i> Birthday:
+              </label>
+
+              <input type="date" id="birthday" {...register("birthday")} />
+
+              <label htmlFor="email">Email:</label>
+
+              <input type="email" id="email" {...register("email")} />
+
+              <label htmlFor="password">Password:</label>
+
+              <input type="password" id="password" {...register("password")} />
+
+              <div className="buttons-create-user-container">
+                <button className="btn btn-create">Create User</button>
+                <button
+                  className="btn btn-cancel"
+                  type="button"
+                  onClick={() => setIsVisible(!isVisible)}
+                >
+                  <i className="fa-solid fa-ban"></i> Cancelar
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <button
+            type="button"
+            className="fresh"
+            onClick={() => apiUsers("https://users-crud1.herokuapp.com/users/")}
+          >
+            <i className="fa-solid fa-arrows-rotate"></i>
+          </button>
+          <button
+            type="button"
+            className="add-user-fixed"
+            onClick={() => setIsVisible(!isVisible)}
+          >
+            <i className="fa-solid fa-plus"></i>{" "}
+          </button>
+        </div>
+      )}
+      <div className="general-container">
+        <h1>Users</h1>
+
+        <ul className="card-user-ul">
+          {users.map((user) => (
+            <Users user={user} apiUsers={apiUsers} key={user.id} />
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Users.jsx
@@ -0,0 +1,145 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import moment from "moment/moment";
+
+const Users = ({ user, apiUsers }) => {
+  const { register, handleSubmit, reset } = useForm();
+  const [isInput, setIsInput] = useState(false);
+
+  // aqui voy a actualizar un usuario
+  const submit = (data) => {
+    apiUsers(
+      `https://users-crud1.herokuapp.com/users/${user.id}/`,
+      "put",
+      data
+    );
+    setIsInput(false);
+  };
+
+  //aqui voy a eliminar un usuario
+
+  const deleteUser = () =>
+    apiUsers(`https://users-crud1.herokuapp.com/users/${user.id}/`, "delete");
+
+  return !isInput ? (
+    <li
+      className="card-user"
+      //   style={{ backgroundColor: user.color }}
+      key={user.id_user}
+    >
+      <h3>
+        {user.first_name} {user.last_name}
+      </h3>
+      <hr />
+      <h4>Email</h4>
+      <p>
+        <i className="fa-solid fa-envelope"></i> {user.email}
+      </p>
+      <h4>Birthday</h4>
+      <p>
+        <i className="fa-solid fa-cake-candles"></i>{" "}
+        {moment(user.birthday).format("MMM DD YYYY")}
+      </p>
+      <hr />
+      <button
+        className="btn btn-edit rigth"
+        type="button"
+        onClick={() => {
+          setIsInput(true);
+
+          reset({
+            first_name: user.first_name,
+            last_name: user.last_name,
+            email: user.email,
+            password: user.password,
+            birthday: user.birthday,
+            // color: user.color,
+          });
+        }}
+      >
+        <i className="fa-regular fa-pen-to-square"></i> Edit
+      </button>
+      <br />
+    </li>
+  ) : (
+    <li
+      className="card-user"
+         style={{ backgroundColor: user.color }}
+      key={user.id_user}
+    >
+      <form className="" onSubmit={handleSubmit(submit)}>
+        <div className="input-form">
+          {" "}
+          <label htmlFor="first_name">First name:</label>
+          <input
+            required
+            type="text"
+            id="first_name"
+            {...register("first_name")}
+          />
+        </div>
+
+        <div className="input-form">
+          <label htmlFor="last_name">Last name:</label>
+
+          <input
+            required
+            type="text"
+            id="last_name"
+            {...register("last_name")}
+          />
+        </div>
+
+        <div className="input-form">
+          <label htmlFor="birthday">
+            <i className="fa-solid fa-cake-candles"></i> Birthday:
+          </label>
+
+          <input type="date" id="birthday" {...register("birthday")} />
+        </div>
+
+        <div className="input-form">
+          {" "}
+          <label htmlFor="password">
+            <i className="fa-solid fa-envelope"></i> Email:
+          </label>
+          <input type="email" id="email" {...register("email")} />
+        </div>
+
+        <div className="input-form">
+          {" "}
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" {...register("password")} />
+        </div>
+
+        {/* <label htmlFor="title">Color:</label>
+        
+        <input
+          className="color-input"
+          type="color"
+          id="color"
+          {...register("color")}
+        />
+         */}
+        <div className="button-form">
+          <button className="btn btn-create">
+            <i className="fa-regular fa-pen-to-square"></i> Update
+          </button>
+          <button
+            className="btn btn-cancel"
+            type="button"
+            onClick={() => setIsInput(false)}
+          >
+            <i className="fa-solid fa-ban"></i> Cancel
+          </button>
+          <button className="btn btn-delete" type="button" onClick={deleteUser}>
+            <i className="fa-solid fa-trash"></i> Delete
+          </button>
+        </div>
+      </form>
+    </li>
+  );
+};
+
+export default Users;
